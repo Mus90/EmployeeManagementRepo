a47be3c1b05fb04bec6724f1cbaffaa950bf92e7
mustafa ali <mustafaalamin.07@gmail.com>
2021-08-07 20:39:01 +0200

    first commit

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EmployeeManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
